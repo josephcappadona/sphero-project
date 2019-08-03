5bd8ff01da9c337b9ef7c06564b251ce532c882b
Julia Chun <hjchun96@gmail.com>
2019-08-03 16:09:45 -0400

    wip for routine

--- src/a_star_routine.py
+++ src/a_star_routine.py
@@ -7,16 +7,21 @@ import maneuver
 
 
 # connect to Sphero
-droid = DroidClient()
-droid.scan()
-droid.connect_to_droid('Q5-8CC0')
-#droid.connect_to_R2D2()
+agent_droid = DroidClient()
+agent_droid.scan()
+agent_droid.connect_to_droid('Q5-8CC0') # Agent
+
+bad_droid_1 = DroidClient()
+bad_droid_1.scan()
+bad_droid_1.connect_to_droid('Q5-ACC0') # Bad Agent 1
+
+bad_droid_2 = DroidClient()
+bad_droid_2.scan()
+bad_droid_2.connect_to_droid('Q5-8CC0') # Bad Agent 2
 
 # get course, find path
 G = courses.grid_1
 start = (0,0)
-goal = (3,0)
-path = A_star(G, start, goal)
 
 # ☑ =start node, ☒ =goal node
 # ☐ ══☐   ☐ ══☐
@@ -26,9 +31,26 @@ path = A_star(G, start, goal)
 # ☐ ══☐   ☐ ══☐
 # ║       ║   ║
 # ☑   ☐ ══☐   ☒
-
-# traverse path
+goal = (3,0)
 speed = 0x88  # half speed
-maneuver.follow_path(droid, path, speed, scale_dist = 0.75)#dist_constant=0.75)
-droid.animate(10)
-droid.quit()
+
+f
+while True:
+
+    #  AGENT
+    path = A_star(G, start, goal)
+    maneuver.follow_path(agent_droid, path, speed, scale_dist = 0.75)#dist_constant=0.75)
+    agent_droid.animate(10)
+    agent_droid.quit()
+
+    # BAD DROID 1
+    path2 = move_enemy(G)
+    maneuver.follow_path(bad_droid_1, path2, speed, scale_dist = 0.75)#dist_constant=0.75)
+    bad_droid_1.animate(10)
+    bad_droid_1.quit()
+
+    # BAD DROID 2
+    path3 = move_enemy(G)
+    maneuver.follow_path(bad_droid_2, path3, speed, scale_dist = 0.75)#dist_constant=0.75)
+    bad_droid_2.animate(10)
+    bad_droid_2.quit()
