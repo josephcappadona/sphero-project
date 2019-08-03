f182fcee1f84da6469661d0bdee139958b5f2ec0
Hunter Kelley <hunterkelley@huntsman-ve703-01447.apn.wlan.private.upenn.edu>
2019-08-03 12:46:33 -0400

    edit to make it work

--- src/a_star_routine.py
+++ src/a_star_routine.py
@@ -8,7 +8,9 @@ import maneuver
 
 # connect to Sphero
 droid = DroidClient()
-droid.connect_to_R2D2()
+droid.scan()
+droid.connect_to_droid('Q5-8CC0')
+#droid.connect_to_R2D2()
 
 # get course, find path
 G = courses.grid_1
@@ -27,6 +29,6 @@ path = A_star(G, start, goal)
 
 # traverse path
 speed = 0x88  # half speed
-maneuver.follow_path(droid, path, speed, dist_constant=0.75)
+maneuver.follow_path(droid, path, speed, scale_dist = 0.75)#dist_constant=0.75)
 droid.animate(10)
 droid.quit()
